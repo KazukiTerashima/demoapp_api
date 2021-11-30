78f8994ed1e5da240702d7cb661bef3fd96a5c3e
KazukiTerashima <terashima.kazuki@outlook.jp>
2021-12-01 08:55:37 +0900

    Add Custom Validation for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,22 @@
+require "validator/email_validator"
+
 class User < ApplicationRecord
+  # top
+  # バリデーション前に実施
+  before_validation :downcase_email
+
   # bcrypt
   has_secure_password
+
   validates :name, presence: true,
                    length: {
                      maximum: 50,
                      allow_blank: true
                    }
+  validates :email, presence: true,
+                    email: {
+                      allow_blank: true
+                    }
   VALID_PASSWORD_REGEX = /\A[\w\-]+\z/
   validates :password, presence: true,
                        length: {
@@ -18,4 +29,20 @@ class User < ApplicationRecord
                          allow_blank: true
                        },
                        allow_nil: true
+  class << self
+    def find_activated(email)
+      find_by(email: email, activated: true)
+    end
+  end
+
+  def email_activated?
+    users = User.where.not(id: id)
+    users.find_activated(email).present?
+  end
+
+  private
+    # email小文字化
+    def downcase_email
+      self.email.downcase! if email
+    end
 end
