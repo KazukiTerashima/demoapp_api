cebfd42debd4a270c5f0f5cacdccbde214ff5937
KazukiTerashima <terashima.kazuki@outlook.jp>
2021-12-16 22:28:57 +0900

    Add Users_contorller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # root "hello#index"
   namespace :api do
     namespace :v1 do
-      resources :hello, only:[:index]
+      resources :users, only:[:index]
     end
   end
 end
